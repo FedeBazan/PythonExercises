e6591ebf4d0dbc307f07d9739f1d93abcba38042
FedeBazan <juliopablofedericobazan@gmail.com>
2023-04-26 12:31:58 -0300

    Fede Bazan
    Update

--- Lesson07_POO/ejercicioAritmetica.py
+++ Lesson07_POO/ejercicioAritmetica.py
@@ -4,16 +4,16 @@ class Aritmetica:
         self.num2=num2
 
     def sumar(self):
-        print(f'la suma entre {self.num1} y {self.num2} es: {self.num1+self.num2}')
+        print(f'la suma {self.num1}+{self.num2}={self.num1+self.num2}')
     
     def restar(self):
-        print(f'La resta entre {self.num1} y {self.num2} es: {self.num1-self.num2}')
+        print(f'La resta {self.num1}-{self.num2}={self.num1-self.num2}')
     
     def multiplicar(self):
-        print(f'La multiplicacion entre {self.num1} y {self.num2} es: {self.num1*self.num2}')
+        print(f'La multiplicacion {self.num1}*{self.num2}={self.num1*self.num2}')
     
     def dividir(self):
-        print(f'La division entre {self.num1} y {self.num2} es: {round(self.num1/self.num2,2)}')
+        print(f'La division entre {self.num1}/{self.num2}={round(self.num1/self.num2,2)}')
     
 operadorA=int(input('Ingresa el primer operador: '))
 operadorB=int(input('Ingresa el segundo operador: '))
