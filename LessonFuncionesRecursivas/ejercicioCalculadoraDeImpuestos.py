956d7aca5efbc6fe4976f3f52162a6902873358a
FedeBazan <juliopablofedericobazan@gmail.com>
2023-04-26 08:33:39 -0300

    d

--- LessonFuncionesRecursivas/ejercicioCalculadoraDeImpuestos.py
+++ LessonFuncionesRecursivas/ejercicioCalculadoraDeImpuestos.py
@@ -4,3 +4,11 @@ Crear una función para calcular el total de un pago incluyendo
 un impuesto aplicado.
 Formula: pago_total=pago_sin_impuesto+pago_sin_impuesto*()
 '''
+def pagoTotal(pago,impuesto){
+        return 
+}
+
+pago=round(float(input('Ingrese el pago sin impuesto: ')),2)
+impuesto=int(input('Ingrese el porcentaje de impuesto a aplicar'))
+
+
